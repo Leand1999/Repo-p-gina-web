5364c9240184b9ace5521a591b41a2d7d54b5423
Leand1999 <143460386+Leand1999@users.noreply.github.com>
2024-09-05 12:32:55 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,1 @@
-const pi=3.14
-function calcular_area_circulo(radio){
-    let circulo= pi * radio**2
-    console.log(circulo);
-}
-
-calcular_area_circulo(4)
-console.log
-
-
-
-
+let nombre="pedro"
